19ae11947f40d7a36dda6a6b158dfc810fd5cb3f
Paul Smith <paul.smith@kcl.ac.uk>
2022-01-12 12:40:21 +0000

    Fix path to plotting

--- signalanalysis/tools/python.py
+++ signalanalysis/tools/python.py
@@ -1,7 +1,7 @@
 import numpy as np
 from typing import List, Tuple, Union, Optional, Any
 
-from signalanalysis import plotting
+from signalanalysis.tools import plotting
 
 def recursive_len(item: list):
     """ Return the total number of elements with a potentially nested list """
